5abf94947914e05a17a395b6e28b29cdef724b08
Mihre15 <tesfayemihretab399@gmail.com>
2025-08-07 09:00:47 +0300

    style commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,8 +18,8 @@
 
 		// Go To Top JS
 		var scrolled = $(window).scrollTop();
-		if (scrolled > 300) $('.go-top').addClass('active');
-		if (scrolled < 300) $('.go-top').removeClass('active');
+		if (scrolled > 500) $('.go-top').addClass('active');
+		if (scrolled < 500) $('.go-top').removeClass('active');
 	});
 
 	// Services Slide JS
